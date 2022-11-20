3a3c9d785020e67c96deaa60cf4811db21453e51
Denis Dunaev <uzzem57@mail.ru>
2022-11-20 23:17:07 +0300

    Добавлен код решения задачи с собакой

---
+++ Seminar_1/Program.cs
@@ -0,0 +1,21 @@
+﻿int count =0;
+int disstance = 10000;
+int first_speed_friend = 1;
+int second_speed_friend = 2;
+int dog_speed = 5;
+int friend = 2;
+int time = 1;
+while (disstance>10) {
+    if (friend ==1) {
+        time = disstance/(first_speed_friend + dog_speed);
+        friend = 2;
+    }
+    else {
+        time = disstance/(second_speed_friend + dog_speed);
+        friend = 1;
+    }
+    disstance = disstance - (first_speed_friend + second_speed_friend)*time;
+    count++;
+
+}
+Console.WriteLine("Собака пробежит " + count + " раз");
