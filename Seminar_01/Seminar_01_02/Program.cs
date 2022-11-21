6a43ded30229e0e6660af074eabd25342fdd9ad6
Denis Dunaev <uzzem57@mail.ru>
2022-11-21 22:16:14 +0300

    Добавлены задачи 2 и 3

---
+++ Seminar_01/Seminar_01_02/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите первое число: ");
+int finish_data = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int data = Convert.ToInt32(Console.ReadLine());
+if (finish_data == data*data) {
+    Console.WriteLine("Да");
+}
+else {
+    Console.WriteLine("Нет");
+}
---
+++ Seminar_01/Seminar_01_03/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Введите  число  от 1 до 7: ");
+int data = Convert.ToInt32(Console.ReadLine());
+if ( data == 1) {
+    Console.WriteLine("Понедельник");
+}
+else if (data == 2) {
+    Console.WriteLine("Вторник");
+}
+else if (data == 3) {
+    Console.WriteLine("Среда");
+}
+else if (data == 4) {
+    Console.WriteLine("Четверг");
+}
+else if (data == 5) {
+    Console.WriteLine("Пятница");
+}
+else if (data == 6) {
+    Console.WriteLine("Суббота");
+}
+else if (data == 7) {
+    Console.WriteLine("Воскресенье");
+}
+else {
+    Console.WriteLine("Введено не корректное число");
+}
+
