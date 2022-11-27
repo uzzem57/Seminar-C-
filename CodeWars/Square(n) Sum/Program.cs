aef2ebc7fb11e6f8a1b1f92b85efab1866994d29
Denis Dunaev <uzzem57@mail.ru>
2022-11-27 23:38:08 +0300

    добавлено решение задачи SquareSum из CodeWar

---
+++ CodeWars/Square(n) Sum/Program.cs
@@ -0,0 +1,17 @@
+﻿
+public static class Kata
+{
+  public static int SquareSum(int[] numbers)
+  { 
+    for ( int i =0; i < numbers.Length; i++) {
+        numbers[i] = numbers[i] * numbers[i];
+    }
+    int count =0;
+    for (int i =0; i < numbers.Length; i++) {
+        count = count + numbers [i];
+    }
+    return count;
+    
+  }
+
+}
